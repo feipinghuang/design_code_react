479951671fa2ec195594260e6dfaf47af2a7ac72
feipinghuang <feiping.huang@thexstore.cn>
2018-08-24 21:15:32 +0800

    fix header when scrolled

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,34 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-const Header = () => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img width="30" src={require('../images/logo-designcode.svg')} /></Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/download">Download</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy" className="BuyButton">Buy</Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  state = {
+    hasScrolled: false
+  }
+
+  componentDidMount () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (e) => {
+    this.setState({
+      hasScrolled: window.pageYOffset > 50
+    })
+  }
+
+  render () {
+    return (
+      <div className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}>
+        <div className="HeaderGroup">
+          <Link to="/"><img width="30" src={require('../images/logo-designcode.svg')} /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/download">Download</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy" className="BuyButton">Buy</Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
