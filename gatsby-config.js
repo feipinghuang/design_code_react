95d0975854a3a6bfc0dc23605a3d5e98ab9f7cce
feipinghuang <feiping.huang@thexstore.cn>
2018-08-27 17:32:03 +0800

    add footer component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Design+Code 3',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'cl0m7gxwwtim',
+        accessToken: '621cc907e02e82f106b97fe28690d7008d121463bca80e37a201150a9b3050c9'
+      }
+    }
+  ],
 }
